825d57ff4e7dbe92f76c8ef40ec4aad5fed689a1
PechMivan <pech.mivan@gmail.com>
2024-01-25 17:57:11 -0600

    adding new changes for Tomcat deploying

--- src/main/java/com/product/springbootapp/Product.java
+++ src/main/java/com/product/springbootapp/Product.java
@@ -1,6 +1,6 @@
 package com.product.springbootapp;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "products")
--- src/main/java/com/product/springbootapp/SpringbootAppApplication.java
+++ src/main/java/com/product/springbootapp/SpringbootAppApplication.java
@@ -2,12 +2,31 @@ package com.product.springbootapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class SpringbootAppApplication {
+public class SpringbootAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootAppApplication.class, args);
+		SpringApplication.run(applicationClass, args);
 	}
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static final Class<SpringbootAppApplication> applicationClass = SpringbootAppApplication.class;
+}
+
+@RestController
+class GreetingController {
+
+	@RequestMapping("/hello/{name}")
+	String hello(@PathVariable String name) {
+		return "Hello, " + name + "!";
+	}
 }
